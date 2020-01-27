bc48205c60402d1e2c0e8c3f5a405bf5388f098a
farooq98 <farooqhd98@gmail.com>
2020-01-27 20:54:06 +0500

    initial commit

---
+++ Unique/Unique Method without using in keyword.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def Unique(lst):
+    try:
+        temp = [lst[0]]
+        a = False
+        for i in lst:
+            a = True
+            for j in temp:
+                if j == i:
+                    a = False
+                    break
+            if a:
+                temp.append(i)
+        return temp
+    except:
+        return "list is empty"
+
+x = [1,2,2,6,3,4,5,4,3,5,6,1,9]
+print(Unique(x))
+
+
+# In[ ]:
+
+
+
+
